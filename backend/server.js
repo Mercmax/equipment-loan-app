06df1456257c6b0c7ea271af331cf1eb5dc04540
Mercmax <mercmax@yandex.ru>
2024-03-16 17:15:35 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const equipmentRoutes = require('./api/routes/equipmentRoutes');
+const userRoutes = require('./api/routes/userRoutes');
 const app = express();
 app.use(bodyParser.json());
-// Define routes here in the future
+// Use the API routes
+app.use('/api', equipmentRoutes);
+app.use('/api', userRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
